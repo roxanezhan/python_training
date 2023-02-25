2ae4702289bf7269fe2fbe04bc6c94ee27735e18
userlan <raushangul714@gmail.com>
2023-02-25 18:09:38 +0600

    added Katalon scenario script

--- test_add_group.py
+++ test_add_group.py
@@ -15,37 +15,37 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("sdfdf")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("dgfdgdfg")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("ghjgh")
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
-    
+        wd.find_element(By.NAME,"user").click()
+        wd.find_element(By.NAME,"user").clear()
+        wd.find_element(By.NAME,"user").send_keys("admin")
+        wd.find_element(By.NAME,"pass").clear()
+        wd.find_element(By.NAME,"pass").send_keys("secret")
+        wd.find_element(By.XPATH,"//input[@value='Login']").click()
+        wd.find_element(By.LINK_TEXT,"groups").click()
+        wd.find_element(By.NAME,"new").click()
+        wd.find_element(By.NAME,"group_name").click()
+        wd.find_element(By.NAME,"group_name").clear()
+        wd.find_element(By.NAME,"group_name").send_keys("testgroup")
+        wd.find_element(By.NAME,"group_header").click()
+        wd.find_element(By.NAME,"group_header").clear()
+        wd.find_element(By.NAME,"group_header").send_keys("testheader")
+        wd.find_element(By.NAME,"group_footer").click()
+        wd.find_element(By.NAME,"group_footer").clear()
+        wd.find_element(By.NAME,"group_footer").send_keys("testfooter")
+        wd.find_element(By.NAME,"submit").click()
+        wd.find_element(By.LINK_TEXT,"group page").click()
+        wd.find_element(By.LINK_TEXT,"Logout").click()
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
+
+    """def is_alert_present(self):
+        try: self.wd.switch_to.alert
         except NoAlertPresentException as e: return False
-        return True
-    
+        return True"""
+
     def tearDown(self):
         self.wd.quit()
 
