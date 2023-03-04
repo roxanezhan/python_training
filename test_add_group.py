00abf6cf73a43eb98d6d25037cd789f765588795
userlan <raushangul714@gmail.com>
2023-03-04 10:58:08 +0600

    the same script as in the course

--- test_add_group.py
+++ test_add_group.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to().alert
+        return True
+    except:
+        return False
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(60)
 
     def test_add_group(self):
+        success = True
         wd = self.wd
         wd.get("http://localhost/addressbook/")
         wd.find_element(By.NAME,"user").click()
         wd.find_element(By.NAME,"user").clear()
         wd.find_element(By.NAME,"user").send_keys("admin")
+        wd.find_element(By.NAME, "pass").click()
         wd.find_element(By.NAME,"pass").clear()
         wd.find_element(By.NAME,"pass").send_keys("secret")
         wd.find_element(By.XPATH,"//input[@value='Login']").click()
@@ -35,16 +41,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element(By.NAME,"submit").click()
         wd.find_element(By.LINK_TEXT,"group page").click()
         wd.find_element(By.LINK_TEXT,"Logout").click()
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-
-    """def is_alert_present(self):
-        try: self.wd.switch_to.alert
-        except NoAlertPresentException as e: return False
-        return True"""
+        self.assertTrue(success)
 
     def tearDown(self):
         self.wd.quit()
