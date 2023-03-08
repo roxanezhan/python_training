0df626400598974b693a171cb81f3c4e813c8f4d
userlan <raushangul714@gmail.com>
2023-03-08 16:39:46 +0600

    3.3 login - second part: добавлена проверка, если был нечайно прописан logout. для этого добавили ensure, которая у меня не работает.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,7 +4,6 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="testgroup1", header="testheader1", footer="testfooter1"))
-    app.session.logout()
 
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
