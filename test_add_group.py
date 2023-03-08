af1a4b8617a613a01751eb49b3d10550be0cf516
userlan <raushangul714@gmail.com>
2023-03-08 09:42:40 +0600

    test_add_group synchronized

--- test_add_group.py
+++ test_add_group.py
@@ -1,5 +1,3 @@
-# этот скрипт пишет разработчик по тестам
-
 # -*- coding: utf-8 -*-
 import pytest
 from group import Group
@@ -8,21 +6,18 @@ from application import Application
 
 # если написать "@pytest.fixture", то наша функция из простой функции превращается в фикстуру
 @pytest.fixture
-def app():
+def app(request):
     fixture = Application()
-    request.addfinalyzer(fixture.destroy)
+    request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def test_add_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="testgroup", header="testheader", footer="testfooter"))
-    app.logout(wd)
+    app.logout()
 
 def test_add_empty_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout(wd)
-
-if __name__ == "__main__":
-    unittest.main()
+    app.logout()
