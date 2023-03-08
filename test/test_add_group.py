2c085cb5c08692be25b25702a392cf8cdb80771c
userlan <raushangul714@gmail.com>
2023-03-08 15:58:16 +0600

    3.3 login - first part

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,6 +4,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="testgroup1", header="testheader1", footer="testfooter1"))
+    app.session.logout()
 
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
